19a27c3b43f6e97c93ad2761d9b17a91e9a50cfd
John Robert Mahlan <johnrobertmahlan@Johns-Air.jetpack>
2020-05-03 14:04:48 -0600

    code for user input

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 
 /*----- app's state (variables) -----*/
 
-let weatherData, fTemp, cTemp;
+let weatherData, userInput, fTemp, cTemp;
 
 
 
@@ -20,6 +20,8 @@ const $weather = $('#weather');
 
 const $temperature = $('#temperature');
 
+const $input = $('input[type="text"]')
+
 
 /*----- event listeners -----*/
 $('form').on('submit', handleGetData);
@@ -30,15 +32,24 @@ $('form').on('submit', handleGetData);
 /*----- functions -----*/
 function handleGetData(event) {
     event.preventDefault();
+
+    userInput = $input.val();
+    $input.val('');
+
     $.ajax({
-        url: 'http://api.openweathermap.org/data/2.5/weather?q=Charlottesville&appid=3130b9f6f3c126f9bc6a4a2c72b3f56c'
+        url: 'http://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=3130b9f6f3c126f9bc6a4a2c72b3f56c'
     }).then(function(data) {
-        $name.html(data.name);
-        $weather.html(data.weather[0].main);
-        fTemp = Math.round(data.main.temp * 9/5 -459.67);
-        cTemp = Math.round(data.main.temp  - 273.15);
-        $temperature.html(fTemp + ' F (' + cTemp + ' C)');
+        weatherData = data;
+        render();
     }, function(error) {
         console.log(error);
     });
 }
+
+function render() {
+    $name.html(weatherData.name);
+    $weather.html(weatherData.weather[0].main);
+    fTemp = Math.round(weatherData.main.temp * 9/5 -459.67);
+    cTemp = Math.round(weatherData.main.temp  - 273.15);
+    $temperature.html(fTemp + ' F (' + cTemp + ' C)');
+};
